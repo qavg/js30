39cd995582eb778eed50ff8de8a28845e35e94e4
qavg <vqa.swe.sp21@gmail.com>
2021-03-17 04:24:22 -0400

    :sparkles: Stop function from running if there's no audio element associated with keycode

--- drum-kit/main.js
+++ drum-kit/main.js
@@ -1,4 +1,4 @@
 window.addEventListener("keydown", function (e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  console.log("🚀 ~ file: main.js ~ line 7 ~ audio", audio);
+  if (!audio) return;
 });
